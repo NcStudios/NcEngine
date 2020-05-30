e93ac2bbfef28cef9959d2ad1ef19e96c190db2d
Cal Romer <cal.romer@gmail.com>
2020-05-29 23:11:05 -0500

    Remove bat files

--- build/imgui.bat
+++
@@ -1,5 +0,0 @@
-cd obj
-g++ -Wall -std=c++17^
- -I..\nc\win32^
- -I..\nc\utils\editor\imgui^
- -c ..\nc\utils\editor\imgui\*.cpp^
--- build/link.bat
+++
@@ -1,1 +0,0 @@
-g++ -Wall -std=c++17 -o app obj\*.o -lgdi32 -ld3d11 -ld3dcompiler_47
