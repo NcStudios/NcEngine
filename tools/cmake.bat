97fb230e428369f54a01e600425eb641d70aff26
Cal Romer <cal.romer@gmail.com>
2020-12-08 19:05:40 -0600

    rework building libraries

--- tools/cmake.bat
+++ tools/cmake.bat
@@ -5,19 +5,33 @@ if "%~1"=="" (
     EXIT
 )
 
-if not exist "%~dp0\..\external\lib\libimgui.a" (
-    call "jobs\build_external_libs.bat"
-)
-
-if not exist "%~dp0\..\external\lib\libenet.a" (
-    call "jobs\build_external_libs.bat"
-)
+call "%~dp0\jobs\build_external_libs.bat" %1
 
 if not exist "%~dp0\..\build" (
     mkdir "%~dp0\..\build"
 )
 
 cd "%~dp0\..\build"
+
+if "%~1"=="Release" (
+    echo Using Release Configuration
+    if not exist "Release" (
+        mkdir "Release"
+    )
+    cd "Release"
+    cmake -DCMAKE_BUILD_TYPE=Release -G "Ninja" "%~dp0\.."
+    EXIT
+)
+
+if "%~1"=="Release-Profile" (
+    echo Using Release Configuration with Profiling
+    if not exist "Release-Profile" (
+        mkdir "Release-Profile"
+    )
+    cd "Release-Profile"
+    cmake -DCMAKE_BUILD_TYPE=Release -DPROFILING_ENABLED=ON -G "Ninja" "%~dp0\.."
+    EXIT
+)
 
 if "%~1"=="Debug" (
     echo Using Debug Configuration
@@ -26,22 +40,17 @@ if "%~1"=="Debug" (
     )
     cd "Debug"
     cmake -DCMAKE_BUILD_TYPE=Debug -DNC_EDITOR_ENABLED=ON -DNC_TESTS_ENABLED=ON -DVERBOSE_LOGGING_ENABLED=ON -G "Ninja" "%~dp0\.."
+    EXIT
 )
 
-if "%~1"=="Release" (
-    echo Using Release Configuration
-    if not exist "Release" (
-        mkdir "Release"
+if "%~1"=="Debug-SanitizeUB" (
+    echo Using Debug Configuration with Undefined Behavior Sanitizer
+    if not exist "Debug-SanitizeUB" (
+        mkdir "Debug-SanitizeUB"
     )
-    cd "Release"
-    cmake -DCMAKE_BUILD_TYPE=Release -DNC_EDITOR_ENABLED=OFF -DNC_TESTS_ENABLED=OFF -DVERBOSE_LOGGING_ENABLED=OFF -G "Ninja" "%~dp0\.."
+    cd "Debug-SanitizeUB"
+    cmake -DCMAKE_BUILD_TYPE=Debug -DNC_EDITOR_ENABLED=ON -DNC_TESTS_ENABLED=ON -DVERBOSE_LOGGING_ENABLED=ON -DSANITIZE_UB=ON -G "Ninja" "%~dp0\.."
+    EXIT
 )
 
-if "%~1"=="Ninja" (
-    echo Using Ninja
-    if not exist "Ninja" (
-        mkdir "Ninja"
-    )
-    cd "Ninja"
-    cmake -DCMAKE_BUILD_TYPE=Debug -DNC_EDITOR_ENABLED=ON -DNC_TESTS_ENABLED=ON -DVERBOSE_LOGGING_ENABLED=ON -G "Ninja" "%~dp0\.."
-)
+echo Error: Unknown build configuration
--- tools/jobs/build_external_libs.bat
+++ tools/jobs/build_external_libs.bat
@@ -1,6 +1,21 @@
-mkdir "%~dp0/../../external_libs_temp_build"
-cd "%~dp0/../../external_libs_temp_build"
-cmake -G "MinGW Makefiles" "%~dp0/../../external"
-mingw32-make
-cd "%~dp0/../.."
-rmdir /s /q external_libs_temp_build
+echo Building External Libraries
+
+if "%~1"=="Debug" goto BuildDebug
+if "%~1"=="Debug-SanitizeUB" goto BuildDebug
+if "%~1"=="Release" goto BuildRelease
+if "%~1"=="Release-Profile" goto BuildRelease
+
+echo Error: Unknown build type(build_external_libs)
+EXIT
+
+:BuildDebug
+    call "%~dp0/build_imgui.bat" Debug
+    call "%~dp0/build_enet.bat" Debug
+    goto Cleanup
+
+:BuildRelease
+    call "%~dp0/build_imgui.bat" Release
+    call "%~dp0/build_enet.bat" Release
+    goto Cleanup
+
+:Cleanup
