c8b0dac8d0114a26d51a47647828b557fe015ad6
Cal Romer <cal.romer@gmail.com>
2020-12-11 03:19:51 -0600

    rework cmake

--- tools/jobs/build_enet.bat
+++ tools/jobs/build_enet.bat
@@ -2,7 +2,7 @@ mkdir "%~dp0/lib_enet_temp_build"
 cd "%~dp0/lib_enet_temp_build"
 
 if "%~1"=="Debug" (
-    if not exist "%~dp0\..\..\external\lib\enet-Debug.a" (
+    if not exist "%~dp0\..\..\external\lib\libenet-Debug.a" (
         echo Building ENet with Debug Configuration
         cmake -DCMAKE_BUILD_TYPE=Debug -G "Ninja" "%~dp0/../../external/src/enet"
         goto Build
@@ -10,7 +10,7 @@ if "%~1"=="Debug" (
 )
 
 if "%~1"=="Release" (
-    if not exist "%~dp0\..\..\external\lib\enet-Release.a" (
+    if not exist "%~dp0\..\..\external\lib\libenet-Release.a" (
         echo Building ENet with Release Configuration
         cmake -DCMAKE_BUILD_TYPE=Release -G "Ninja" "%~dp0/../../external/src/enet"
         goto Build
