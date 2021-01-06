3210cbabf856385ba9c992407bdc2ecaf0d04ac9
Cal Romer <cal.romer@gmail.com>
2021-01-06 17:31:18 -0600

    remove comment

--- nc/source/graphics/rendergraph/Step.h
+++ nc/source/graphics/rendergraph/Step.h
@@ -10,7 +10,6 @@ namespace nc::graphics
     {
         public:
             Step(size_t targetPass);
-            //void Submit(class FrameManager& frame, const nc::graphics::Model& model) const noexcept;
 
             size_t targetPass; /** @todo: Replace with enum or do templating **/
     };
