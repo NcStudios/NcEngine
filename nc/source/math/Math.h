9730fbbc832b2b98d87997cbc99885a839bb5196
Cal Romer <cal.romer@gmail.com>
2021-01-18 18:59:53 -0600

    use numbers::pi

--- nc/source/math/Math.h
+++ nc/source/math/Math.h
@@ -4,16 +4,13 @@
 #include <cstring>
 #include <concepts>
 #include <cmath>
-#include <limits> //numeric_limits::epsilon
-
-#include <iostream>
+#include <limits>
+#include <numbers>
 
 namespace nc::math
 {
     constexpr auto FloatEpsilon = std::numeric_limits<float>::epsilon();
     constexpr auto DoubleEpsilon = std::numeric_limits<double>::epsilon();
-    constexpr double PI_DOUBLE = atan(1)*4;
-    constexpr float  PI_FLOAT  = PI_DOUBLE;
 
     template<std::totally_ordered T> constexpr T Min(T a, T b) noexcept { return a < b ? a : b; }
     template<std::totally_ordered T> constexpr T Max(T a, T b) noexcept { return a > b ? a : b; }
@@ -35,22 +32,22 @@ namespace nc::math
 
     constexpr double WrapAngle(double theta) noexcept
     {
-        const double modTwoPi = fmod(theta, 2.0L * PI_DOUBLE);
-        return (modTwoPi > PI_DOUBLE ? (modTwoPi - 2.0L * PI_DOUBLE) : modTwoPi);
+        const double modTwoPi = std::fmod(theta, 2.0L * std::numbers::pi);
+        return (modTwoPi > std::numbers::pi ? (modTwoPi - 2.0L * std::numbers::pi) : modTwoPi);
     }
 
-    constexpr double DegreeToRadian(double degrees) noexcept { return degrees * PI_DOUBLE / 180.0;     }
-    constexpr double RadianToDegree(double radians) noexcept { return radians *     180.0 / PI_DOUBLE; }
+    constexpr double DegreeToRadian(double degrees) noexcept { return degrees * std::numbers::pi / 180.0; }
+    constexpr double RadianToDegree(double radians) noexcept { return radians * 180.0 / std::numbers::pi; }
 
     constexpr bool FloatEqual(float a, float b, float maxAbsDiff = 0.00001f, float maxRelDiff = FloatEpsilon) noexcept
     {
         if(a == b)
             return true;
-        auto diff = fabs(a - b);
+        auto diff = std::fabs(a - b);
         if(diff <= maxAbsDiff)
             return true;
-        auto aAbs = fabs(a);
-        auto bAbs = fabs(b);
+        auto aAbs = std::fabs(a);
+        auto bAbs = std::fabs(b);
         auto max = (aAbs > bAbs) ? aAbs : bAbs;
         if(diff <= max * maxRelDiff)
             return true;
