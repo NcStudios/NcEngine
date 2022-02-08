9eee3643a781462fc0f6b26eb924cdb87ae5b535
McCallister Romer <cal.romer@gmail.com>
2022-02-07 23:38:00 -0600

    InvokeFreeComponent supports all logic types

--- nc/include/ecs/InvokeFreeComponent.h
+++ nc/include/ecs/InvokeFreeComponent.h
@@ -4,26 +4,39 @@
 
 namespace nc
 {
-    /** @todo It may be useful to allow invoking fixed and collision logic as well. */
-
-   /** Helper for invoking a FreeComponent's Run method from a FrameLogic
-    *  component. The user-defined constructor can be used to add an instance
-    *  of T to the registry. */
+   /** Helper for invoking a FreeComponent's Run method from one of the logic components. */
     template<std::derived_from<FreeComponent> T>
     struct InvokeFreeComponent
     {
+        /** The default constructor expects the component to be added externally. */
         InvokeFreeComponent() = default;
 
+        /** The user-defined constructor attaches an instance of the component to be invoked. */
         template<class ... Args>
         InvokeFreeComponent(Entity self, Registry* registry, Args&&... args)
         {
             registry->Add<T>(self, std::forward<Args>(args)...);
         }
 
+        /** FrameLogic call operator */
         void operator()(Entity self, Registry* registry, float dt) const
         {
-            if(auto* attachment = registry->Get<T>(self))
-                attachment->Run(self, registry, dt);
+            if(auto* component = registry->Get<T>(self))
+                component->Run(self, registry, dt);
+        }
+
+        /** FixedLogic call operator */
+        void operator()(Entity self, Registry* registry) const
+        {
+            if(auto* component = registry->Get<T>(self))
+                component->Run(self, registry);
+        }
+
+        /** CollisionLogic call operator */
+        void operator()(Entity self, Entity hit, Registry* registry) const
+        {
+            if(auto* component = registry->Get<T>(self))
+                component->Run(self, hit, registry);
         }
     };
 }
