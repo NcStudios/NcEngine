1cf188258dc04e3433463d6757cd942cc6f54081
jaremieromer <jaremieromer@outlook.com>
2022-03-27 17:54:57 -0500

    Progress on build script prior to fixing debug issue.

---
+++ tools/build.ps1
@@ -0,0 +1,51 @@
+param([string]$TargetName, [string]$Generator, [string]$PresetModifier="")
+
+function Run-Commands {
+    param (
+        [string]$SourceDirectory,
+        [string]$PresetName
+    )
+
+    cmake -S "$SourceDirectory/" -B "build/$PresetName" --preset "$PresetName"
+    cmake --build "build/$PresetName"
+    if ($PresetName.Contains("NcEngine"))
+    {
+        Write-Host "Installing build/$PresetName"
+        cmake --install "build/$PresetName"
+    }
+}
+
+$sourceDirectory = ""
+$presetName = ""
+$requiresEngine = $false
+
+$presetName = "$TargetName-$Generator-$PresetModifier"
+
+if ($TargetName.Equals("NcEngine")) {
+    $sourceDirectory = "engine/"
+    $requiresEngine = $false
+}
+elseif ($TargetName.Equals("Sample")) {
+    $sourceDirectory = "sample/"
+    $requiresEngine = $true
+}
+elseif ($TargetName.Equals("NcEditor")) {
+    $sourceDirectory = "editor/"
+    $requiresEngine = $true
+}
+elseif ($TargetName.Equals("Tests")) {
+    $sourceDirectory = "test/"
+    $requiresEngine = $false
+    $presetName = "$TargetName-$Generator"
+}
+
+
+if ($requiresEngine) {
+    Run-Commands -SourceDirectory "engine/" -PresetName "NcEngine-$Generator-$PresetModifier"
+}
+
+Write-Host "PresetName: $presetName"
+Write-Host "SourceDirectory: $sourceDirectory"
+Run-Commands -SourceDirectory $sourceDirectory -PresetName $presetName 
+
+Write-Host "Complete"
