706a9877e630be539acb099ff1419598395d1fd1
McCallister Romer <cal.romer@gmail.com>
2022-07-30 22:26:54 -0500

    comments

--- sdk/engine/include/module/Module.h
+++ sdk/engine/include/module/Module.h
@@ -6,18 +6,19 @@ namespace nc
 {
     struct Job;
 
-    /** @brief Base class for modules that attach workloads to the core execution graph. */
+    /** @brief Modules are extensions that provide functionality to the engine. */
     struct Module
     {
         virtual ~Module() = default;
 
         /**
-         * @brief Builds a list of jobs required by the module.
+         * @brief Builds a list of jobs required by the module. Returns an empty vector by default.
          * @return std::vector<Job>
          */
         virtual auto BuildWorkload() -> std::vector<Job>;
 
-        /** @brief Called on scene changes and NcEngine::Shutdown(). */
+        /** @brief Called prior to clearing the module's associated data registry. This includes
+         *  scene changes and NcEngine::Shutdown(). */
         virtual void Clear() noexcept;
     };
 }
