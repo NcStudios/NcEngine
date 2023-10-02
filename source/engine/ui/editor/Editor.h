d478267563b39df0e3762a35ce4e339f824a77aa
McCallisterRomer <47375742+McCallisterRomer@users.noreply.github.com>
2023-10-02 15:28:06 -0500

    Adding more CI job variants (#430)
    * try to add to matrix
    
    * try different matrix setup
    
    * clean up names
    
    * missing value
    
    * define editor funcs in prod build
    
    * add profiling to one build

--- source/engine/ui/editor/Editor.h
+++ source/engine/ui/editor/Editor.h
@@ -1,21 +1,31 @@
-#ifdef NC_EDITOR_ENABLED
 #pragma once
 
 #include "ecs/Registry.h"
 
 namespace nc::ui::editor
 {
-    class Editor
-    {
-        public:
-            Editor();
-            void Frame(float* dt, Registry* registry);
+class Editor
+{
+    public:
+        Editor();
+        void Frame(float* dt, Registry* registry);
 
-        private:
-            bool m_openState_Editor;
-            bool m_openState_UtilitiesPanel;
+    private:
+        bool m_openState_Editor;
+        bool m_openState_UtilitiesPanel;
 
-            void DrawMenu();
-    };
+        void DrawMenu();
+};
+
+// Provide stubs if editor is disabled
+// TODO: #427 Make a proper interface + factory for editor
+#ifndef NC_EDITOR_ENABLED
+Editor::Editor()
+{
+}
+
+void Editor::Frame(float*, Registry*)
+{
 }
 #endif
+} // namespace nc::ui::editor
