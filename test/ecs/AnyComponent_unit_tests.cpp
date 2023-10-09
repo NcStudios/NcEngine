a4434cb8bcdd092517dd344b6767e1a327be0e47
McCallisterRomer <cal.romer@gmail.com>
2023-10-09 07:49:00 -0500

    some tests

--- test/ecs/AnyComponent_unit_tests.cpp
+++ test/ecs/AnyComponent_unit_tests.cpp
@@ -17,6 +17,31 @@ TEST(AnyComponentTest, OperatorBool_nullValue_returnsFalse)
     EXPECT_FALSE(static_cast<bool>(uut));
 }
 
+TEST(AnyComponentTest, Name_nameSet_retrievesValue)
+{
+    constexpr auto expected = "test";
+    auto component = TestComponent{};
+    auto handler = nc::ComponentHandler<TestComponent>{ .name = expected };
+    auto uut = nc::AnyComponent{&component, &handler};
+    EXPECT_EQ(expected, uut.Name());
+}
+
+TEST(AnyComponentTest, HasDrawUI_funcSet_returnsTrue)
+{
+    auto component = TestComponent{};
+    auto handler = nc::ComponentHandler<TestComponent>{ .drawUI = [](TestComponent&){} };
+    auto uut = nc::AnyComponent{&component, &handler};
+    EXPECT_TRUE(uut.HasDrawUI());
+}
+
+TEST(AnyComponentTest, HasDrawUI_funcNotSet_returnsFalse)
+{
+    auto component = TestComponent{};
+    auto handler = nc::ComponentHandler<TestComponent>{ .drawUI = nullptr };
+    auto uut = nc::AnyComponent{&component, &handler};
+    EXPECT_FALSE(uut.HasDrawUI());
+}
+
 TEST(AnyComponentTest, DrawUI_validCall_dispatchesToHandler)
 {
     auto numCalls = 0;
@@ -30,4 +55,12 @@ TEST(AnyComponentTest, DrawUI_validCall_dispatchesToHandler)
     uut.DrawUI();
     uut.DrawUI();
     EXPECT_EQ(2, numCalls);
+}
+
+TEST(AnyComponentTest, DrawUI_nullFunc_succeeds)
+{
+    auto component = TestComponent{};
+    auto handler = nc::ComponentHandler<TestComponent>{ .drawUI = nullptr };
+    auto uut = nc::AnyComponent{&component, &handler};
+    EXPECT_NO_THROW(uut.DrawUI());
 }
