4963cf1744fc1f3ea618b5e8ea6509889d294559
McCallisterRomer <47375742+McCallisterRomer@users.noreply.github.com>
2023-10-12 16:35:00 -0500

    In-place AnyComponent construction (#447)
    * in-place AnyComponent
    
    * destruct on assignment
    
    * cleanup
    
    * fix test
    
    * code review

--- include/ncengine/ecs/detail/AnyComponentUtility.h
+++ include/ncengine/ecs/detail/AnyComponentUtility.h
@@ -6,11 +6,15 @@
 
 namespace nc::detail
 {
+class AnyImplStorage;
+
 struct AnyImplBase
 {
     virtual ~AnyImplBase() = default;
-    virtual auto Name() -> std::string_view = 0;
-    virtual auto HasDrawUI() const -> bool = 0;
+    virtual auto Clone(AnyImplStorage& dest) const noexcept -> AnyImplBase* = 0;
+    virtual auto MoveTo(AnyImplStorage& dest) noexcept -> AnyImplBase* = 0;
+    virtual auto Name() const noexcept -> std::string_view = 0;
+    virtual auto HasDrawUI() const noexcept -> bool = 0;
     virtual void DrawUI() = 0;
 };
 
@@ -19,30 +23,173 @@ class AnyImplConcrete : public AnyImplBase
 {
     public:
         explicit AnyImplConcrete(T* instance, ComponentHandler<T>* handler) noexcept
-            : m_instance{instance}, m_handler{handler}
-        {
-        }
+            : m_instance{instance}, m_handler{handler} {}
 
-        auto Name() -> std::string_view override
-        {
-            return m_handler->name;
-        }
-
-        auto HasDrawUI() const -> bool override
-        {
-            return m_handler->drawUI != nullptr;
-        }
-
-        void DrawUI() override
-        {
-            if (HasDrawUI())
-            {
-                m_handler->drawUI(*m_instance);
-            }
-        }
+        auto Clone(AnyImplStorage& dest) const noexcept -> AnyImplBase* override;
+        auto MoveTo(AnyImplStorage& dest) noexcept -> AnyImplBase* override;
+        auto Name() const noexcept -> std::string_view override;
+        auto HasDrawUI() const noexcept -> bool override;
+        void DrawUI() override;
 
     private:
         T* m_instance;
         ComponentHandler<T>* m_handler;
 };
+
+class AnyImplStorage
+{
+    public:
+        constexpr AnyImplStorage() noexcept
+            : buffer{} {}
+
+        template<PooledComponent T>
+        AnyImplStorage(T* instance, ComponentHandler<T>* handler);
+
+        ~AnyImplStorage() noexcept;
+        AnyImplStorage(const AnyImplStorage& other) noexcept;
+        AnyImplStorage(AnyImplStorage&& other) noexcept;
+        auto operator=(const AnyImplStorage& other) noexcept -> AnyImplStorage&;
+        auto operator=(AnyImplStorage&& other) noexcept -> AnyImplStorage&;
+
+        auto operator==(const AnyImplStorage& other) const noexcept
+        {
+            return std::ranges::equal(buffer, other.buffer);
+        }
+
+        auto HasValue() const noexcept -> bool
+        {
+            return std::ranges::any_of(buffer, [](auto b) { return b != std::byte{}; });
+        }
+
+        auto AsImpl() noexcept -> AnyImplBase*
+        {
+            return reinterpret_cast<AnyImplBase*>(buffer);
+        }
+
+        auto AsImpl() const noexcept -> const AnyImplBase*
+        {
+            return reinterpret_cast<const AnyImplBase*>(buffer);
+        }
+
+        auto AsStorage() noexcept -> void*
+        {
+            return buffer;
+        }
+
+    private:
+        struct Any{};
+        using Placeholder = detail::AnyImplConcrete<Any>;
+        static constexpr auto align = alignof(Placeholder);
+        static constexpr auto size = sizeof(Placeholder);
+
+        alignas(align) std::byte buffer[size];
+
+        void Clear() noexcept;
+};
+
+template<PooledComponent T>
+auto AnyImplConcrete<T>::Clone(AnyImplStorage& dest) const noexcept -> AnyImplBase*
+{
+    return new (dest.AsStorage()) AnyImplConcrete<T>{*this};
+}
+
+template<PooledComponent T>
+auto AnyImplConcrete<T>::MoveTo(AnyImplStorage& dest) noexcept -> AnyImplBase*
+{
+    return new (dest.AsStorage()) AnyImplConcrete<T>{std::move(*this)};
+}
+
+template<PooledComponent T>
+auto AnyImplConcrete<T>::Name() const noexcept -> std::string_view
+{
+    return m_handler->name;
+}
+
+template<PooledComponent T>
+auto AnyImplConcrete<T>::HasDrawUI() const noexcept -> bool
+{
+    return m_handler->drawUI != nullptr;
+}
+
+template<PooledComponent T>
+void AnyImplConcrete<T>::DrawUI()
+{
+    m_handler->drawUI(*m_instance);
+}
+
+template<PooledComponent T>
+AnyImplStorage::AnyImplStorage(T* instance, ComponentHandler<T>* handler)
+{
+    NC_ASSERT(instance && handler, "AnyComponent params must not be null, use AnyComponent() instead.")
+    using Impl = detail::AnyImplConcrete<T>;
+
+    // Only place we know the derived Impl type - validate all of our assumptions.
+    static_assert(sizeof(Impl) == size &&
+                  alignof(Impl) == align &&
+                  std::has_virtual_destructor_v<Impl> &&
+                  std::is_nothrow_destructible_v<Impl> &&
+                  std::is_nothrow_copy_constructible_v<Impl> &&
+                  std::is_nothrow_move_constructible_v<Impl> &&
+                  std::is_nothrow_copy_assignable_v<Impl> &&
+                  std::is_nothrow_move_assignable_v<Impl>);
+
+    new (buffer) Impl{instance, handler};
+}
+
+inline AnyImplStorage::~AnyImplStorage() noexcept
+{
+    if (HasValue())
+    {
+        AsImpl()->~AnyImplBase();
+    }
+}
+
+inline AnyImplStorage::AnyImplStorage(const AnyImplStorage& other) noexcept
+    : buffer{}
+{
+    if (other.HasValue())
+    {
+        other.AsImpl()->Clone(*this);
+    }
+}
+
+inline AnyImplStorage::AnyImplStorage(AnyImplStorage&& other) noexcept
+    : buffer{}
+{
+    if (other.HasValue())
+    {
+        other.AsImpl()->MoveTo(*this);
+    }
+}
+
+inline auto AnyImplStorage::operator=(const AnyImplStorage& other) noexcept -> AnyImplStorage&
+{
+    Clear();
+    if (other.HasValue())
+    {
+        other.AsImpl()->Clone(*this);
+    }
+
+    return *this;
+}
+
+inline auto AnyImplStorage::operator=(AnyImplStorage&& other) noexcept -> AnyImplStorage&
+{
+    Clear();
+    if (other.HasValue())
+    {
+        other.AsImpl()->MoveTo(*this);
+    }
+
+    return *this;
+}
+
+inline void AnyImplStorage::Clear() noexcept
+{
+    if (HasValue())
+    {
+        AsImpl()->~AnyImplBase();
+        std::memset(buffer, 0, size);
+    }
+}
 } // namespace nc::detail
--- test/ecs/AnyComponent_unit_tests.cpp
+++ test/ecs/AnyComponent_unit_tests.cpp
@@ -2,6 +2,122 @@
 #include "gtest/gtest.h"
 
 struct TestComponent{};
+struct AlternativeComponent{};
+
+TEST(AnyComponentTest, Constructor_nullParams_throws)
+{
+    auto actualComponent = TestComponent{};
+    auto actualHandler = nc::ComponentHandler<TestComponent>{};
+    TestComponent* nullComponent = nullptr;
+    nc::ComponentHandler<TestComponent>* nullHandler = nullptr;
+    EXPECT_THROW(nc::AnyComponent(nullComponent, nullHandler), nc::NcError);
+    EXPECT_THROW(nc::AnyComponent(nullComponent, &actualHandler), nc::NcError);
+    EXPECT_THROW(nc::AnyComponent(&actualComponent, nullHandler), nc::NcError);
+}
+
+TEST(AnyComponentTest, CopyOperations_objectsValidAndEqual)
+{
+    // Test valid object copy
+    {
+        auto component = TestComponent{};
+        auto handler = nc::ComponentHandler<TestComponent>{};
+        const auto source = nc::AnyComponent{&component, &handler};
+        auto copy = source; // copy construct
+        EXPECT_TRUE(copy);
+        EXPECT_TRUE(source);
+        EXPECT_EQ(source, copy);
+        copy = source; // copy assign
+        EXPECT_TRUE(copy);
+        EXPECT_TRUE(source);
+        EXPECT_EQ(source, copy);
+    }
+
+    // Test null object copy
+    {
+        auto source = nc::AnyComponent{};
+        auto copy = nc::AnyComponent{};
+        EXPECT_FALSE(source);
+        EXPECT_FALSE(copy);
+        EXPECT_EQ(source, copy);
+        copy = source;
+        EXPECT_FALSE(source);
+        EXPECT_FALSE(copy);
+        EXPECT_EQ(source, copy);
+    }
+}
+
+TEST(AnyComponentTest, MoveOperations_objectsValid)
+{
+    // Test valid object move
+    {
+        auto component = TestComponent{};
+        auto handler = nc::ComponentHandler<TestComponent>{};
+        auto source = nc::AnyComponent{&component, &handler};
+        // Keep a backup for comparison since we're moving. Its ptrs being moved, so
+        // maybe not necessary, but feels weird comparing to a moved-from value.
+        auto backup = source;
+        auto moved = std::move(source); // move construct
+        EXPECT_TRUE(moved);
+        EXPECT_TRUE(source);
+        EXPECT_EQ(backup, moved);
+        moved = nc::AnyComponent{&component, &handler}; // move assign
+        EXPECT_TRUE(moved);
+        EXPECT_EQ(backup, moved);
+    }
+
+    // Test null object move
+    {
+        auto source = nc::AnyComponent{};
+        auto backup = source; // overkill at this point
+        auto moved = std::move(source);
+        EXPECT_FALSE(source);
+        EXPECT_FALSE(moved);
+        EXPECT_EQ(backup, moved);
+        moved = nc::AnyComponent{};
+        EXPECT_FALSE(source);
+        EXPECT_EQ(backup, moved);
+    }
+}
+
+TEST(AnyComponentTest, EqualityOperators_equal_compareEqual)
+{
+    auto component = TestComponent{};
+    auto handler = nc::ComponentHandler<TestComponent>{};
+    const auto lhs = nc::AnyComponent{&component, &handler};
+    const auto rhs = nc::AnyComponent{&component, &handler};
+    EXPECT_TRUE(lhs == rhs);
+    EXPECT_FALSE(lhs != rhs);
+}
+
+TEST(AnyComponentTest, EqualityOperators_notEqual_compareNotEqual)
+{
+    auto component1 = TestComponent{};
+    auto component2 = TestComponent{};
+    auto handler = nc::ComponentHandler<TestComponent>{};
+    const auto lhs = nc::AnyComponent{&component1, &handler};
+    const auto rhs = nc::AnyComponent{&component2, &handler};
+    EXPECT_FALSE(lhs == rhs);
+    EXPECT_TRUE(lhs != rhs);
+}
+
+TEST(AnyComponentTest, EqualityOperators_differentTypes_compareNotEqual)
+{
+    auto component1 = TestComponent{};
+    auto component2 = AlternativeComponent{};
+    auto handler1 = nc::ComponentHandler<TestComponent>{};
+    auto handler2 = nc::ComponentHandler<AlternativeComponent>{};
+    const auto lhs = nc::AnyComponent{&component1, &handler1};
+    const auto rhs = nc::AnyComponent{&component2, &handler2};
+    EXPECT_FALSE(lhs == rhs);
+    EXPECT_TRUE(lhs != rhs);
+}
+
+TEST(AnyComponentTest, EqualityOperators_nullObjects_compareEqual)
+{
+    const auto lhs = nc::AnyComponent{};
+    const auto rhs = nc::AnyComponent{};
+    EXPECT_TRUE(lhs == rhs);
+}
 
 TEST(AnyComponentTest, OperatorBool_hasValue_returnsTrue)
 {
@@ -62,5 +178,12 @@ TEST(AnyComponentTest, DrawUI_nullFunc_succeeds)
     auto component = TestComponent{};
     auto handler = nc::ComponentHandler<TestComponent>{ .drawUI = nullptr };
     auto uut = nc::AnyComponent{&component, &handler};
+    EXPECT_FALSE(uut.HasDrawUI());
     EXPECT_NO_THROW(uut.DrawUI());
+}
+
+TEST(AnyComponentTest, DrawUI_nullObject_throws)
+{
+    auto uut = nc::AnyComponent{};
+    EXPECT_THROW(uut.DrawUI(), nc::NcError);
 }
