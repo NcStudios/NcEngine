6bded0dba1e173be2c62bd2bc71376a931601116
McCallisterRomer <cal.romer@gmail.com>
2023-10-20 15:22:34 -0500

    popup can't have transparency

--- source/engine/ui/ImGuiStyle.cpp
+++ source/engine/ui/ImGuiStyle.cpp
@@ -11,9 +11,9 @@ void SetDefaultUIStyle()
     ImVec4* colors = style.Colors;
     colors[ImGuiCol_Text]                  = default_scheme::Text;
     colors[ImGuiCol_TextDisabled]          = default_scheme::Disabled;
-    colors[ImGuiCol_WindowBg]              = default_scheme::PrimaryBG;
-    colors[ImGuiCol_ChildBg]               = default_scheme::SecondaryBG;
-    colors[ImGuiCol_PopupBg]               = default_scheme::SecondaryBG;
+    colors[ImGuiCol_WindowBg]              = default_scheme::WindowBG;
+    colors[ImGuiCol_ChildBg]               = default_scheme::ChildBG;
+    colors[ImGuiCol_PopupBg]               = default_scheme::PopupBG;
     colors[ImGuiCol_Border]                = default_scheme::Border;
     colors[ImGuiCol_BorderShadow]          = default_scheme::AccentTrans;
     colors[ImGuiCol_FrameBg]               = default_scheme::Title;
@@ -22,7 +22,7 @@ void SetDefaultUIStyle()
     colors[ImGuiCol_TitleBg]               = default_scheme::Title;
     colors[ImGuiCol_TitleBgActive]         = default_scheme::Title;
     colors[ImGuiCol_TitleBgCollapsed]      = default_scheme::Title;
-    colors[ImGuiCol_MenuBarBg]             = default_scheme::SecondaryBG;
+    colors[ImGuiCol_MenuBarBg]             = default_scheme::ChildBG;
     colors[ImGuiCol_ScrollbarBg]           = default_scheme::ScrollBarBG;
     colors[ImGuiCol_ScrollbarGrab]         = default_scheme::Accent;
     colors[ImGuiCol_ScrollbarGrabHovered]  = default_scheme::AccentActive;
@@ -33,7 +33,7 @@ void SetDefaultUIStyle()
     colors[ImGuiCol_Button]                = default_scheme::Button;
     colors[ImGuiCol_ButtonHovered]         = default_scheme::Hovered;
     colors[ImGuiCol_ButtonActive]          = default_scheme::Active;
-    colors[ImGuiCol_Header]                = default_scheme::PrimaryBG;
+    colors[ImGuiCol_Header]                = default_scheme::WindowBG;
     colors[ImGuiCol_HeaderHovered]         = default_scheme::Tabs;
     colors[ImGuiCol_HeaderActive]          = default_scheme::Hovered;
     colors[ImGuiCol_Separator]             = default_scheme::Separator;
