afb1102a7cc028645f824a5b23fd33409f2bf5d1
McCallisterRomer <47375742+McCallisterRomer@users.noreply.github.com>
2023-10-21 20:24:47 -0500

    Adding header with forward declarations (#470)
    * add fwd file
    
    * only include what is needed

---
+++ include/ncengine/NcFwd.h
@@ -0,0 +1,67 @@
+/**
+ * @file NcFwd.h
+ * @copyright Jaremie and McCallister Romer 2023
+ */
+#pragma once
+
+namespace nc
+{
+class NcEngine;
+class Registry;
+class ModuleRegistry;
+class Entity;
+struct Module;
+class AnyComponent;
+class ComponentBase;
+class FreeComponent;
+class CollisionLogic;
+class FixedLogic;
+class FrameLogic;
+class Tag;
+class Transform;
+struct Quaternion;
+struct Vector2;
+struct Vector3;
+struct Vector4;
+
+namespace audio
+{
+struct NcAudio;
+class AudioSource;
+} // namespace audio
+
+namespace config
+{
+struct Config;
+struct AssetSettings;
+struct AudioSettings;
+struct MemorySettings;
+struct GraphicsSettings;
+struct PhysicsSettings;
+struct ProjectSettings;
+} // namespace config
+
+namespace graphics
+{
+struct NcGraphics;
+class Camera;
+class MeshRenderer;
+class ParticleEmitter;
+class PointLight;
+class SceneNavigationCamera;
+class ToonRenderer;
+} // namespace graphics
+
+namespace net
+{
+class NetworkDispatcher;
+} // namespace net
+
+namespace physics
+{
+struct NcPhysics;
+class Collider;
+class ConcaveCollider;
+class PhysicsBody;
+} // namespace physics
+} // namespac nc
