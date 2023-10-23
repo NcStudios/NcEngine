ccb9aa72dd84ce003b64b0e0b8b17a2b949180af
McCallisterRomer <cal.romer@gmail.com>
2023-10-23 15:36:32 -0500

    take variadic args in stubs

--- include/ncengine/utility/Log.h
+++ include/ncengine/utility/Log.h
@@ -14,14 +14,14 @@
     #define NC_LOG_ERROR(str, ...)        NC_LOG_IMPL('E', str NC_OPT_EXPAND(__VA_ARGS__));
     #define NC_LOG_EXCEPTION(exception)   nc::utility::detail::Log(exception);
 #else
-    #define NC_LOG_INFO(str);
-    #define NC_LOG_WARNING(str);
-    #define NC_LOG_ERROR(str);
+    #define NC_LOG_INFO(str, ...);
+    #define NC_LOG_WARNING(str, ...);
+    #define NC_LOG_ERROR(str, ...);
     #define NC_LOG_EXCEPTION(exception);
 #endif
 
 #if NC_LOG_LEVEL >= 2
     #define NC_LOG_TRACE(str, ...)        NC_LOG_IMPL('V', str NC_OPT_EXPAND(__VA_ARGS__));
 #else
-    #define NC_LOG_TRACE(str);
+    #define NC_LOG_TRACE(str, ...);
 #endif
