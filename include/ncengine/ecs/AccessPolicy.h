91773fb517d4a7fa3485520d7e793ccee1cf4f86
McCallisterRomer <cal.romer@gmail.com>
2023-12-20 07:22:27 -0600

    add policy method

--- include/ncengine/ecs/AccessPolicy.h
+++ include/ncengine/ecs/AccessPolicy.h
@@ -29,14 +29,6 @@ enum class FilterBase
 template<class T>
 concept RegistryType = Component<T> || std::same_as<Entity, T>;
 
-/** @brief Specifies an AccessPolicy includes all requested types. */
-template<class Policy, class... Includes>
-constexpr auto HasAccess = (Policy::FilterType::template included<Includes> && ...);
-
-/** @brief Specifies a policy is convertible to another policy with the requested ruleset. */
-template<class Policy, FilterBase Base, class... Includes>
-constexpr auto PolicyConvertibleTo = Policy::base >= Base && HasAccess<Policy, Includes...>;
-
 /** @brief Specifies a type is invocable with a data pool of type T. */
 template<class F, class T>
 concept PoolInvocable = std::invocable<F, decltype(std::declval<ComponentRegistry*>()->GetPool<T>())>;
@@ -78,13 +70,25 @@ class AccessPolicy
                 detail::MatchFilter<Entity, Tag, Transform, detail::FreeComponentGroup, Includes...>,
                 detail::MatchFilter<detail::FreeComponentGroup, Includes...>>>;
 
+        /** @brief Indicates whether all requested types are accessible by the policy. */
+        template<class... TargetIncludes>
+        static constexpr bool HasAccess = (FilterType::template included<TargetIncludes> && ...);
+
+        /** @brief Indicates the policy's FilterBase is at least as permissive as the requested base. */
+        template<FilterBase TargetBase>
+        static constexpr bool BaseContains = Base >= TargetBase;
+
+        /** @brief Indicates whether the policy is convertible to another policy with the requested ruleset. */
+        template<FilterBase TargetBase, class... TargetIncludes>
+        static constexpr bool ConvertibleTo = BaseContains<TargetBase> && HasAccess<TargetIncludes...>;
+
         /** @brief Construct an AccessPolicy object. */
         AccessPolicy(ComponentRegistry& registry) noexcept
             : m_registry{&registry} {}
 
         /** @brief Implicit conversion operator to a more restricted policy. */
         template<FilterBase TargetBase, class... TargetIncludes>
-            requires PolicyConvertibleTo<AccessPolicy<Base, Includes...>, TargetBase, TargetIncludes...>
+            requires ConvertibleTo<TargetBase, TargetIncludes...>
         operator AccessPolicy<TargetBase, TargetIncludes...>() const noexcept
         {
             return AccessPolicy<TargetBase, TargetIncludes...>{*m_registry};
@@ -92,10 +96,17 @@ class AccessPolicy
 
         /** @brief Invoke a callable with the requested data pool. */
         template<RegistryType T, PoolInvocable<T> F>
-            requires HasAccess<AccessPolicy<Base, Includes...>, T>
+            requires HasAccess<T>
         auto OnPool(F&& func) const -> decltype(auto)
         {
             return func(m_registry->GetPool<T>());
+        }
+
+        /** @brief Get a range of pointers to all ComponentPoolBase instances. */
+        auto GetComponentPools()
+            requires BaseContains<FilterBase::All>
+        {
+            return m_registry->GetComponentPools();
         }
 
     private:
--- test/ecs/AccessPolicy_unit_tests.cpp
+++ test/ecs/AccessPolicy_unit_tests.cpp
@@ -10,46 +10,46 @@ using allPolicy = nc::ecs::AccessPolicy<nc::ecs::FilterBase::All>;
 
 TEST(AccessPolicyTests, PolicyConvertibleTo_identity_isTrue)
 {
-    static_assert(nc::ecs::PolicyConvertibleTo<explicitPolicy, nc::ecs::FilterBase::None, S1, S2>);
-    static_assert(nc::ecs::PolicyConvertibleTo<basicPolicy, nc::ecs::FilterBase::Basic, S1, S2>);
-    static_assert(nc::ecs::PolicyConvertibleTo<allPolicy, nc::ecs::FilterBase::All>);
+    static_assert(explicitPolicy::ConvertibleTo<nc::ecs::FilterBase::None, S1, S2>);
+    static_assert(basicPolicy::ConvertibleTo<nc::ecs::FilterBase::Basic, S1, S2>);
+    static_assert(allPolicy::ConvertibleTo<nc::ecs::FilterBase::All>);
 }
 
 TEST(AccessPolicyTests, PolicyConvertibleTo_moreRestrictiveBase_isTrue)
 {
-    static_assert(nc::ecs::PolicyConvertibleTo<basicPolicy, nc::ecs::FilterBase::None, S1, S2>);
-    static_assert(nc::ecs::PolicyConvertibleTo<allPolicy, nc::ecs::FilterBase::Basic, S1>);
+    static_assert(basicPolicy::ConvertibleTo<nc::ecs::FilterBase::None, S1, S2>);
+    static_assert(allPolicy::ConvertibleTo<nc::ecs::FilterBase::Basic, S1>);
 }
 
 TEST(AccessPolicyTests, PolicyConvertibleTo_lessRestrictiveBase_isFalse)
 {
-    static_assert(!nc::ecs::PolicyConvertibleTo<explicitPolicy, nc::ecs::FilterBase::Basic, S1, S2>);
-    static_assert(!nc::ecs::PolicyConvertibleTo<basicPolicy, nc::ecs::FilterBase::All, S1, S2>); // tparams are redundant but allowed
+    static_assert(!explicitPolicy::ConvertibleTo<nc::ecs::FilterBase::Basic, S1, S2>);
+    static_assert(!basicPolicy::ConvertibleTo<nc::ecs::FilterBase::All, S1, S2>); // tparams are redundant but allowed
 }
 
 TEST(AccessPolicyTests, PolicyConvertibleTo_moreRestrictiveTypeList_isTrue)
 {
-    static_assert(nc::ecs::PolicyConvertibleTo<explicitPolicy, nc::ecs::FilterBase::None>);
-    static_assert(nc::ecs::PolicyConvertibleTo<basicPolicy, nc::ecs::FilterBase::Basic>);
+    static_assert(explicitPolicy::ConvertibleTo<nc::ecs::FilterBase::None>);
+    static_assert(basicPolicy::ConvertibleTo<nc::ecs::FilterBase::Basic>);
 }
 
 TEST(AccessPolicyTests, PolicyConvertibleTo_lessRestrictiveTypeList_isFalse)
 {
-    static_assert(!nc::ecs::PolicyConvertibleTo<explicitPolicy, nc::ecs::FilterBase::None, S1, S2, int>);
-    static_assert(!nc::ecs::PolicyConvertibleTo<basicPolicy, nc::ecs::FilterBase::Basic, S1, S2, int>);
+    static_assert(!explicitPolicy::ConvertibleTo<nc::ecs::FilterBase::None, S1, S2, int>);
+    static_assert(!basicPolicy::ConvertibleTo<nc::ecs::FilterBase::Basic, S1, S2, int>);
 }
 
-TEST(AccessPolicyTests, HasAccess_allAllowed_isTrue)
+TEST(AccessPolicyTests, PolicyAccessible_allAllowed_isTrue)
 {
-    static_assert(nc::ecs::HasAccess<explicitPolicy, S1, S2>);
-    static_assert(nc::ecs::HasAccess<basicPolicy, S1, S2, nc::Entity, nc::Transform, nc::Tag, nc::ecs::detail::FreeComponentGroup>);
-    static_assert(nc::ecs::HasAccess<allPolicy, S1, S2, nc::Entity, nc::Transform, nc::Tag, nc::ecs::detail::FreeComponentGroup, int>);
+    static_assert(explicitPolicy::HasAccess<S1, S2>);
+    static_assert(basicPolicy::HasAccess<S1, S2, nc::Entity, nc::Transform, nc::Tag, nc::ecs::detail::FreeComponentGroup>);
+    static_assert(allPolicy::HasAccess<S1, S2, nc::Entity, nc::Transform, nc::Tag, nc::ecs::detail::FreeComponentGroup, int>);
 }
 
-TEST(AccessPolicyTests, HasAccess_anyNotAllowed_isFalse)
+TEST(AccessPolicyTests, PolicyAccessible_anyNotAllowed_isFalse)
 {
-    static_assert(!nc::ecs::HasAccess<explicitPolicy, nc::Entity, nc::Transform, nc::Tag, nc::ecs::detail::FreeComponentGroup, int>);
-    static_assert(!nc::ecs::HasAccess<basicPolicy, int>);
+    static_assert(!explicitPolicy::HasAccess<nc::Entity, nc::Transform, nc::Tag, nc::ecs::detail::FreeComponentGroup, int>);
+    static_assert(!basicPolicy::HasAccess<int>);
 }
 
 TEST(AccessPolicyTests, OnPool_returnsReference_preservesValueCategory)
@@ -63,4 +63,17 @@ TEST(AccessPolicyTests, OnPool_returnsReference_preservesValueCategory)
     );
 
     static_assert(std::is_lvalue_reference_v<returned>);
+}
+
+template<class T>
+concept CanGetPools = requires (T t)
+{
+    t.GetComponentPools();
+};
+
+TEST(AccessPolicyTests, GetComponentPools_xxxxx)
+{
+    static_assert(CanGetPools<allPolicy>);
+    static_assert(!CanGetPools<basicPolicy>);
+    static_assert(!CanGetPools<explicitPolicy>);
 }
