340c9e225799f25a284f2e27cdfb1300e4207d2d
McCallisterRomer <cal.romer@gmail.com>
2024-01-09 20:48:04 -0600

    tests

--- test/module/ModuleRegistry_unit_tests.cpp
+++ test/module/ModuleRegistry_unit_tests.cpp
@@ -5,6 +5,9 @@ using namespace nc;
 
 struct TestModule1 : public Module
 {
+    static constexpr size_t expectedId = 1ull;
+
+    TestModule1() : Module{expectedId} {}
     void Clear() noexcept override { cleared = true; }
     bool cleared = false;
 };
@@ -13,6 +16,11 @@ struct TestModule2 : public Module
 {
     void Clear() noexcept override { cleared = true; }
     bool cleared = false;
+};
+
+struct TestModuleBadId : public Module
+{
+    TestModuleBadId() : Module{TestModule1::expectedId} {}
 };
 
 TEST(ModuleRegistry_unit_tests, Register_NewModule_RegistersModule)
@@ -39,6 +47,29 @@ TEST(ModuleRegistry_unit_tests, Register_ExistingModule_ReplacesModule)
     EXPECT_EQ(uut.GetAllModules().size(), 1);
 }
 
+TEST(ModuleRegistry_unit_tests, Register_HasExplicitId_DoesNotModifyId)
+{
+    auto uut = ModuleRegistry{};
+    uut.Register<TestModule1>(std::make_unique<TestModule1>());
+    const auto actual = uut.Get<TestModule1>();
+    EXPECT_EQ(TestModule1::expectedId, actual->Id());
+}
+
+TEST(ModuleRegistry_unit_tests, Register_HasDefaultId_AssignsAvailableid)
+{
+    auto uut = ModuleRegistry{};
+    uut.Register<TestModule2>(std::make_unique<TestModule2>());
+    const auto actual = uut.Get<TestModule2>();
+    EXPECT_EQ(std::numeric_limits<size_t>::max(), actual->Id());
+}
+
+TEST(ModuleRegistry_unit_tests, Register_IdAlreadyInUse_Throws)
+{
+    auto uut = ModuleRegistry{};
+    uut.Register<TestModule1>(std::make_unique<TestModule1>());
+    EXPECT_THROW(uut.Register<TestModuleBadId>(std::make_unique<TestModuleBadId>()), NcError);
+}
+
 TEST(ModuleRegistry_unit_tests, Unregister_ExistingModule_Unregisters)
 {
     auto uut = ModuleRegistry{};
@@ -58,6 +89,32 @@ TEST(ModuleRegistry_unit_tests, Unregister_NonexistentModule_Succeeds)
     EXPECT_FALSE(uut.IsRegistered<TestModule1>());
     EXPECT_EQ(uut.Get<TestModule1>(), nullptr);
     EXPECT_EQ(uut.GetAllModules().size(), 0);
+}
+
+TEST(ModuleRegistry_unit_tests, IsRegistered_ModuleRegistered_ReturnsTrue)
+{
+    auto uut = ModuleRegistry{};
+
+    // clear any static pts from previous tests
+    uut.Unregister<TestModule1>();
+    uut.Unregister<TestModule2>();
+
+    uut.Register<TestModule1>(std::make_unique<TestModule1>());
+    EXPECT_TRUE(uut.IsRegistered<TestModule1>());
+    EXPECT_TRUE(uut.IsRegistered(TestModule1::expectedId));
+}
+
+TEST(ModuleRegistry_unit_tests, IsRegistered_ModuleNotRegistered_ReturnsFalse)
+{
+    auto uut = ModuleRegistry{};
+
+    // clear any static pts from previous tests
+    uut.Unregister<TestModule1>();
+    uut.Unregister<TestModule2>();
+
+    uut.Register<TestModule2>(std::make_unique<TestModule2>());
+    EXPECT_FALSE(uut.IsRegistered<TestModule1>());
+    EXPECT_FALSE(uut.IsRegistered(TestModule1::expectedId));
 }
 
 int main(int argc, char ** argv)
