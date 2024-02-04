d2e5c4bda546dfc2f8333e4f7349882cb050cd7e
Jaremie Romer <33360084+jaremieromer@users.noreply.github.com>
2024-02-03 20:08:59 -0600

    Added padding to 128

--- source/engine/graphics/shader_resource/EnvironmentData.h
+++ source/engine/graphics/shader_resource/EnvironmentData.h
@@ -18,6 +18,8 @@ struct GlobalData
     alignas(16)Vector3 cameraWorldPos = Vector3(0.0f, 0.0f, 0.0f);
     alignas(16)Vector3 ambientColor = Vector3(0.992f, 0.909f, 0.737f);
     alignas(4)uint32_t skyboxTextureIndex = 0u;
+    alignas(16)Vector4 padding0 = Vector4::Zero();
+    alignas(16)Vector4 padding1 = Vector4::Zero();
 };
 
 constexpr auto sizeOfData = alignof(Vector3);
