913b0fc222bc1df67e232f4eae298a4c4690f71f
McCallisterRomer <cal.romer@gmail.com>
2024-02-10 14:36:46 -0600

    bug in matrix order

--- include/ncengine/ecs/Transform.h
+++ include/ncengine/ecs/Transform.h
@@ -49,7 +49,7 @@ class Transform final : public ComponentBase
                   DirectX::FXMMATRIX parentTransform)
             : ComponentBase(entity),
               m_localMatrix{ComposeMatrix(scale, rot, pos)},
-              m_worldMatrix{parentTransform * m_localMatrix}
+              m_worldMatrix{m_localMatrix * parentTransform}
         {
             NC_ASSERT(!HasAnyZeroElement(scale), "Invalid scale(elements cannot be 0)");
         }
