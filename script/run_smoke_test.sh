8969e2b8ef758d35b2f241d888856a762096901b
McCallisterRomer <cal.romer@gmail.com>
2024-05-11 08:35:14 -0500

    update smoke test script

--- script/run_smoke_test.sh
+++ script/run_smoke_test.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-ICD_FILE_PATH="$1"
+if [ "$#" -ne 3 ]; then
+    echo "usage: $0 <ICD_FILE_PATH> <INSTALL_DIR> <OUT_FILE>"
+    exit 1
+fi
 
-export VK_ICD_FILENAMES="$ICD_FILE_PATH=vk_swiftshader_icd.json"
+ICD_FILE_PATH="$1"
+INSTALL_DIR="$2"
+OUT_FILE="$3"
+
+export VK_ICD_FILENAMES="$ICD_FILE_PATH"
 
 echo "VK_ICD_FILENAMES: $VK_ICD_FILENAMES"
+echo "INSTALL_DIR: $INSTALL_DIR"
+echo "OUT_FILE: $OUT_FILE"
 
-./Sample.exe --run-test > "./SampleResults.txt" 2>&1
+cd "$INSTALL_DIR/sample"
+./Sample --run-test > "$OUT_FILE" 2>&1
 EXIT_CODE=$?
-echo $EXIT_CODE >> "./SampleResults.txt"
+echo $EXIT_CODE
+echo $EXIT_CODE >> "$OUT_FILE"
 exit $EXIT_CODE
