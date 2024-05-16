e5d0a4faac0ffee2b494d096cbb757c0903ef285
McCallisterRomer <cal.romer@gmail.com>
2024-05-16 12:00:49 -0500

    try VK_DRIVER_FILES instead

--- script/run_smoke_test.sh
+++ script/run_smoke_test.sh
@@ -14,16 +14,16 @@ echo "ENGINE_INSTALL_DIR: $ENGINE_INSTALL_DIR"
 echo "OUT_FILE: $OUT_FILE"
 
 cd "$ENGINE_INSTALL_DIR/sample"
-cp "$SWIFTSHADER_INSTALL_DIR/vk_swiftshader_icd.json" .
-#cp "$SWIFTSHADER_INSTALL_DIR/vk_swiftshader_driver.json" .
+#cp "$SWIFTSHADER_INSTALL_DIR/vk_swiftshader_icd.json" .
+cp "$SWIFTSHADER_INSTALL_DIR/vk_swiftshader_driver.json" .
 cp "$SWIFTSHADER_INSTALL_DIR/vk_swiftshader.dll" .
-export VK_ICD_FILENAMES="./vk_swiftshader_icd.json"
-#export VK_DRIVER_FILES=c:/dev/NcEngine/install/swiftshader/vk_swiftshader_driver.json
+#export VK_ICD_FILENAMES="./vk_swiftshader_icd.json"
+export VK_DRIVER_FILES=./vk_swiftshader_driver.json
 
-echo $VK_ICD_FILENAMES
-cat $VK_ICD_FILENAMES
-#echo $VK_DRIVER_FILES
-#cat $VK_DRIVER_FILES
+#echo $VK_ICD_FILENAMES
+#cat $VK_ICD_FILENAMES
+echo $VK_DRIVER_FILES
+cat $VK_DRIVER_FILES
 
 # cp "$SWIFTSHADER_INSTALL_DIR/vulkan-1.dll" .
 
