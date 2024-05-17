9620de72d871873a1ce89bc3b19b134b188887af
McCallisterRomer <cal.romer@gmail.com>
2024-05-17 14:20:56 -0500

    disable smoke test stuff

--- sample/source/scenes/SmokeTest.cpp
+++ sample/source/scenes/SmokeTest.cpp
@@ -68,6 +68,8 @@ void SmokeTest::Load(ecs::Ecs world, ModuleProvider modules)
 
     std::cerr << "SmokeTest::Load\n";
 
+    (void)world;
+    (void)modules;
     static auto isSecondPass = false;
     world.Emplace<FrameLogic>(
         world.Emplace<Entity>({}),
@@ -97,82 +99,82 @@ void SmokeTest::Load(ecs::Ecs world, ModuleProvider modules)
         }
     );
 
-    if (isSecondPass)
-    {
-        std::cerr << "second pass unload assets\n";
+    // if (isSecondPass)
+    // {
+    //     std::cerr << "second pass unload assets\n";
 
-        asset::UnloadAllAudioClipAssets();
-        asset::UnloadAllConcaveColliderAssets();
-        asset::UnloadAllConvexHullAssets();
-        asset::UnloadAllCubeMapAssets();
-        asset::UnloadAllMeshAssets();
-        asset::UnloadAllTextureAssets();
-        asset::UnloadAllSkeletalAnimationAssets();
-        std::cerr << "second pass load scene\n";
+    //     asset::UnloadAllAudioClipAssets();
+    //     asset::UnloadAllConcaveColliderAssets();
+    //     asset::UnloadAllConvexHullAssets();
+    //     asset::UnloadAllCubeMapAssets();
+    //     asset::UnloadAllMeshAssets();
+    //     asset::UnloadAllTextureAssets();
+    //     asset::UnloadAllSkeletalAnimationAssets();
+    //     std::cerr << "second pass load scene\n";
 
-        ::LoadScene(world, *modules.Get<asset::NcAsset>());
-    }
-    else
-    {
-        std::cerr << "first pass one time init\n";
+    //     ::LoadScene(world, *modules.Get<asset::NcAsset>());
+    // }
+    // else
+    // {
+    //     std::cerr << "first pass one time init\n";
 
-        const auto ground = world.Emplace<Entity>({
-            .position = Vector3::Up() * -2.5f,
-            .scale = Vector3{3.0f, 1.0f, 3.0f}
-        });
+    //     const auto ground = world.Emplace<Entity>({
+    //         .position = Vector3::Up() * -2.5f,
+    //         .scale = Vector3{3.0f, 1.0f, 3.0f}
+    //     });
 
-        world.Emplace<graphics::ToonRenderer>(ground);
-        auto& groundCollider = world.Emplace<physics::Collider>(ground, physics::BoxProperties{});
-        auto& groundTransform = world.Get<Transform>(ground);
-        world.Emplace<physics::PhysicsBody>(ground, groundTransform, groundCollider);
-        world.Emplace<physics::PositionClamp>(ground, groundTransform.Position(), 0.1f, 2.0f);
-    }
+    //     world.Emplace<graphics::ToonRenderer>(ground);
+    //     auto& groundCollider = world.Emplace<physics::Collider>(ground, physics::BoxProperties{});
+    //     auto& groundTransform = world.Get<Transform>(ground);
+    //     world.Emplace<physics::PhysicsBody>(ground, groundTransform, groundCollider);
+    //     world.Emplace<physics::PositionClamp>(ground, groundTransform.Position(), 0.1f, 2.0f);
+    // }
 
-    std::cerr << "adding camera\n";
-    const auto cameraHandle = world.Emplace<Entity>({.position = Vector3{0.0f, 0.0f, -15.0f}});
-    auto& camera = world.Emplace<graphics::Camera>(cameraHandle);
-    auto ncGraphics = modules.Get<graphics::NcGraphics>();
-    std::cerr << "setting camera\n";
-    ncGraphics->SetCamera(&camera);
-    std::cerr << "setting skybox\n";
-    ncGraphics->SetSkybox(asset::DefaultSkyboxCubeMap);
-    modules.Get<audio::NcAudio>()->RegisterListener(cameraHandle);
+    // std::cerr << "adding camera\n";
+    // const auto cameraHandle = world.Emplace<Entity>({.position = Vector3{0.0f, 0.0f, -15.0f}});
+    // auto& camera = world.Emplace<graphics::Camera>(cameraHandle);
+    // auto ncGraphics = modules.Get<graphics::NcGraphics>();
+    // std::cerr << "setting camera\n";
+    // ncGraphics->SetCamera(&camera);
+    // std::cerr << "setting skybox\n";
+    // ncGraphics->SetSkybox(asset::DefaultSkyboxCubeMap);
+    // modules.Get<audio::NcAudio>()->RegisterListener(cameraHandle);
 
-    const auto object = world.Emplace<Entity>({});
-    world.Emplace<audio::AudioSource>(
-        object,
-        std::vector<std::string>{
-            std::string{asset::DefaultAudioClip}
-        },
-        audio::AudioSourceProperties{
-            .flags = audio::AudioSourceFlags::Play |
-                     audio::AudioSourceFlags::Loop
-        }
-    );
+    // const auto object = world.Emplace<Entity>({});
+    // world.Emplace<audio::AudioSource>(
+    //     object,
+    //     std::vector<std::string>{
+    //         std::string{asset::DefaultAudioClip}
+    //     },
+    //     audio::AudioSourceProperties{
+    //         .flags = audio::AudioSourceFlags::Play |
+    //                  audio::AudioSourceFlags::Loop
+    //     }
+    // );
 
-    world.Emplace<graphics::MeshRenderer>(object);
-    world.Emplace<graphics::ToonRenderer>(object);
-    world.Emplace<graphics::SkeletalAnimator>(object, "DefaultCube.nca", "DefaultCubeAnimation.nca");
-    world.Emplace<graphics::PointLight>(object);
-    world.Emplace<graphics::ParticleEmitter>(object, graphics::ParticleInfo{
-        .emission = {
-            .initialEmissionCount = 10,
-            .periodicEmissionCount = 10,
-            .periodicEmissionFrequency = 0.1f
-        },
-        .init = {},
-        .kinematic = {}
-    });
+    // world.Emplace<graphics::MeshRenderer>(object);
+    // world.Emplace<graphics::ToonRenderer>(object);
+    // world.Emplace<graphics::SkeletalAnimator>(object, "DefaultCube.nca", "DefaultCubeAnimation.nca");
+    // world.Emplace<graphics::PointLight>(object);
+    // world.Emplace<graphics::ParticleEmitter>(object, graphics::ParticleInfo{
+    //     .emission = {
+    //         .initialEmissionCount = 10,
+    //         .periodicEmissionCount = 10,
+    //         .periodicEmissionFrequency = 0.1f
+    //     },
+    //     .init = {},
+    //     .kinematic = {}
+    // });
 
-    std::cerr << "adding physics components\n";
+    // std::cerr << "adding physics components\n";
 
-    auto& objectCollider = world.Emplace<physics::Collider>(object, physics::BoxProperties{});
-    auto& objectTransform = world.Get<Transform>(object);
-    world.Emplace<physics::PhysicsBody>(object, objectTransform, objectCollider);
-    world.Emplace<physics::PhysicsMaterial>(object);
-    world.Emplace<physics::VelocityRestriction>(object, Vector3::One(), Vector3::Up());
+    // auto& objectCollider = world.Emplace<physics::Collider>(object, physics::BoxProperties{});
+    // auto& objectTransform = world.Get<Transform>(object);
+    // world.Emplace<physics::PhysicsBody>(object, objectTransform, objectCollider);
+    // world.Emplace<physics::PhysicsMaterial>(object);
+    // world.Emplace<physics::VelocityRestriction>(object, Vector3::One(), Vector3::Up());
 
-    std::cerr << "adding child\n";
-    world.Emplace<Entity>({.parent = object});
+    // std::cerr << "adding child\n";
+    // world.Emplace<Entity>({.parent = object});
 }
 } // namespace nc::sample
