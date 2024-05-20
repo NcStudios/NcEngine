c2741bd9bfad36fd8fa2b0c6c364e18659c98af9
McCallisterRomer <cal.romer@gmail.com>
2024-05-20 10:04:25 -0500

    modify smoke test script for linix

--- script/run_smoke_test.sh
+++ script/run_smoke_test.sh
@@ -15,23 +15,32 @@ echo "OUT_FILE: $OUT_FILE"
 
 cd "$ENGINE_INSTALL_DIR/sample"
 
-cat <<'EOF' > vk_swiftshader_driver.json
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    SWIFTSHADER_LIB=vk_swiftshader.dll
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    SWIFTSHADER_LIB=libvk_swiftshader.so
+else
+  echo "Unknown OS: $OSTYPE"
+  exit 1
+fi
+
+cat <<EOF > vk_swiftshader_icd.json
 {
   "file_format_version": "1.0.0",
   "ICD": {
-    "library_path": "./vk_swiftshader.dll",
+    "library_path": "$SWIFTSHADER_INSTALL_DIR/$SWIFTSHADER_LIB",
     "api_version": "1.3"
   }
 }
 EOF
 
-cp "$SWIFTSHADER_INSTALL_DIR/vk_swiftshader.dll" .
-export VK_DRIVER_FILES="$ENGINE_INSTALL_DIR/sample/vk_swiftshader_driver.json"
+export VK_DRIVER_FILES="$ENGINE_INSTALL_DIR/sample/vk_swiftshader_icd.json"
 
 echo "VK_DRIVER_FILES: " $VK_DRIVER_FILES
+echo "generated driver manifest:"
 cat $VK_DRIVER_FILES
 
 ./Sample --run-test --config-path smoke_test_config.ini > "$OUT_FILE" 2>&1
 EXIT_CODE=$?
-echo $EXIT_CODE
+echo "smoke test exit code: $EXIT_CODE"
 exit $EXIT_CODE
