690c3e3622f04441501299646f2b101c3ce0c124
Jaremie Romer <jaremieromer@outlook.com>
2024-09-15 21:41:48 -0500

    Engine initialization working for all APIs, windows only so far.

--- source/engine/graphics2/NcGraphicsImpl2.cpp
+++ source/engine/graphics2/NcGraphicsImpl2.cpp
@@ -77,22 +77,17 @@ void RotateElementToBeginning(std::vector<T>& vectorToRotate, const T& elem)
 {
     auto elemPos = std::ranges::find(vectorToRotate, elem);
     NC_ASSERT(elemPos != vectorToRotate.end(), fmt::format("{0} was not present in the vector.", elem));
-    // Store the element before modifying the vector
     T element = *elemPos;
-
-    // Erase the element from its current position
     vectorToRotate.erase(elemPos);
-
-    // Insert the element at the beginning
     vectorToRotate.insert(vectorToRotate.begin(), element);
 }
 
 auto GetSupportedRenderDeviceTypeByPlatform(std::string_view targetApi) -> Diligent::RENDER_DEVICE_TYPE
 {
-    constexpr std::string_view D3D12 = std::string_view("d3d12");
+    constexpr std::string_view D3D12  = std::string_view("d3d12");
     constexpr std::string_view Vulkan = std::string_view("vulkan");
-    constexpr std::string_view D3D11 = std::string_view("d3d11");
-    constexpr std::string_view OpenGL = std::string_view("openGL");
+    constexpr std::string_view D3D11  = std::string_view("d3d11");
+    constexpr std::string_view OpenGL = std::string_view("opengl");
 
     auto preferredWin32ApiOrder = std::vector<std::string_view> 
     {
@@ -109,15 +104,14 @@ auto GetSupportedRenderDeviceTypeByPlatform(std::string_view targetApi) -> Dilig
     };
 
     #if PLATFORM_WIN32
-        // D3D12 already in front if targetApi is D3D12
         if (targetApi == Vulkan)
             RotateElementToBeginning(preferredWin32ApiOrder, Vulkan);
         else if (targetApi == D3D11)
             RotateElementToBeginning(preferredWin32ApiOrder, D3D11);
         else if (targetApi == OpenGL)
             RotateElementToBeginning(preferredWin32ApiOrder, OpenGL);
-        else
-            throw nc::NcError(fmt::format("Target API of {0} is not in the list of potential APIs. Potential APIs: d3d12, vulkan, d3d11, openGL", targetApi));
+        else if (targetApi != D3D12) // D3D12 already in front if targetApi is D3D12
+            throw nc::NcError(fmt::format("Target API of {0} is not in the list of potential APIs. Potential APIs: d3d12, vulkan, d3d11, opengl", targetApi));
 
         for (const auto& api : preferredWin32ApiOrder)
         {
@@ -146,16 +140,15 @@ auto GetSupportedRenderDeviceTypeByPlatform(std::string_view targetApi) -> Dilig
                 #endif
             }
         }
-        throw nc::NcError("No supported API found from [d3d12, vulkan, d3d11, openGL]. Platform: Win32");
+        throw nc::NcError("No supported API found from [d3d12, vulkan, d3d11, opengl]. Platform: Win32");
 
     #elif PLATFORM_LINUX
-        // Vulkan already in front if targetApi is Vulkan
         if (targetApi == D3D12 || targetApi == D3D11)
             NC_LOG_WARNING(fmt::format("Target API of {0} is not supported on Linux. Defaulting to Linux-supported API.", targetApi));
         else if (targetApi == OpenGL)
             RotateElementToBeginning(preferredLinuxApiOrder, OpenGL);
-        else
-            throw nc::NcError(fmt::format("Target API of {0} is not in the list of potential APIs. Potential APIs: vulkan, openGL", targetApi));
+        else if (targetApi != Vulkan)  // Vulkan already in front if targetApi is Vulkan
+            throw nc::NcError(fmt::format("Target API of {0} is not in the list of potential APIs. Potential APIs: vulkan, opengl", targetApi));
 
         for (const auto& api : preferredLinuxApiOrder)
         {
@@ -172,7 +165,7 @@ auto GetSupportedRenderDeviceTypeByPlatform(std::string_view targetApi) -> Dilig
                 #endif
             }
         }
-        throw nc::NcError("No supported API found from [vulkan, openGL]. Platform: Linux");
+        throw nc::NcError("No supported API found from [vulkan, opengl]. Platform: Linux");
     #else
         #error "Unsupported platform detected. NcEngine only supports Win32 and Linux."
     #endif
@@ -228,25 +221,62 @@ NcGraphicsImpl2::NcGraphicsImpl2(const config::GraphicsSettings& graphicsSetting
 
     ImGui::CreateContext();
 
+    /* Initialize Diligent Engine */
+    SwapChainDesc SCDesc;
     auto supportedDeviceTypeByPlatform = GetSupportedRenderDeviceTypeByPlatform(graphicsSettings.targetApi);
 
     switch (supportedDeviceTypeByPlatform)
     {
+        case RENDER_DEVICE_TYPE_D3D12:
+        {
+            auto GetEngineFactoryD3D12 = LoadGraphicsEngineD3D12();
+            EngineD3D12CreateInfo engineCI;
+            auto* pFactoryD3D12 = GetEngineFactoryD3D12();
+            pFactoryD3D12->CreateDeviceAndContextsD3D12(engineCI, &m_pDevice, &m_pImmediateContext);
+            auto win32Handle = glfwGetWin32Window(window.GetWindowHandle());
+            Win32NativeWindow win32Window{win32Handle};
+            pFactoryD3D12->CreateSwapChainD3D12(m_pDevice, m_pImmediateContext, SCDesc, FullScreenModeDesc{}, win32Window, &m_pSwapChain);
+            break;
+        }
         case RENDER_DEVICE_TYPE_VULKAN:
         {
-            
+            auto GetEngineFactoryVk = LoadGraphicsEngineVk();
+            EngineVkCreateInfo engineCI;
+            auto* pFactoryVk = GetEngineFactoryVk();
+            pFactoryVk->CreateDeviceAndContextsVk(engineCI, &m_pDevice, &m_pImmediateContext);
+            auto win32Handle = glfwGetWin32Window(window.GetWindowHandle());
+            if (!m_pSwapChain && win32Handle != nullptr)
+            {
+                Win32NativeWindow win32Window{win32Handle};
+                pFactoryVk->CreateSwapChainVk(m_pDevice, m_pImmediateContext, SCDesc, win32Window, &m_pSwapChain);
+            }
+            break;
         }
+        case RENDER_DEVICE_TYPE_D3D11:
+        {
+            auto* GetEngineFactoryD3D11 = LoadGraphicsEngineD3D11();
+            EngineD3D11CreateInfo engineCI;
+            auto* pFactoryD3D11 = GetEngineFactoryD3D11();
+            pFactoryD3D11->CreateDeviceAndContextsD3D11(engineCI, &m_pDevice, &m_pImmediateContext);
+            auto win32Handle = glfwGetWin32Window(window.GetWindowHandle());
+            Win32NativeWindow win32Window{win32Handle};
+            pFactoryD3D11->CreateSwapChainD3D11(m_pDevice, m_pImmediateContext, SCDesc, FullScreenModeDesc{}, win32Window, &m_pSwapChain);
+            break;
+        }
+        case RENDER_DEVICE_TYPE_GL:
+        {
+            auto GetEngineFactoryOpenGL = LoadGraphicsEngineOpenGL();
+            auto* pFactoryOpenGL = GetEngineFactoryOpenGL();
+            EngineGLCreateInfo engineCI;
+            auto win32Handle = glfwGetWin32Window(window.GetWindowHandle());
+            Win32NativeWindow win32Window{win32Handle};
+            engineCI.Window.hWnd = win32Handle;
+            pFactoryOpenGL->CreateDeviceAndSwapChainGL(engineCI, &m_pDevice, &m_pImmediateContext, SCDesc, &m_pSwapChain);
+            break;
+        }
+        default:
+            throw nc::NcError("Failed to initialize the render device. Unsupported device type.");
     }
-
-    /* Initialize Diligent Engine */
-    SwapChainDesc SCDesc;
-    auto GetEngineFactoryD3D12 = LoadGraphicsEngineD3D12();
-    EngineD3D12CreateInfo engineCI;
-    auto* pFactoryD3D12 = GetEngineFactoryD3D12();
-    pFactoryD3D12->CreateDeviceAndContextsD3D12(engineCI, &m_pDevice, &m_pImmediateContext);
-    auto win32Handle = glfwGetWin32Window(window.GetWindowHandle());
-    Win32NativeWindow win32Window{win32Handle};
-    pFactoryD3D12->CreateSwapChainD3D12(m_pDevice, m_pImmediateContext, SCDesc, FullScreenModeDesc{}, win32Window, &m_pSwapChain);
 }
 
 NcGraphicsImpl2::~NcGraphicsImpl2()
