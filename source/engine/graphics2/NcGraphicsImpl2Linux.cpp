384cb6a5edc830b2e92aa9a61d56aa6ce4731302
Jaremie Romer <33360084+jaremieromer@users.noreply.github.com>
2024-10-06 12:44:56 -0500

    Initial Diligent Engine Integration (#719)

---
+++ source/engine/graphics2/NcGraphicsImpl2Linux.cpp
@@ -0,0 +1,37 @@
+
+#include "ncengine/graphics/NcGraphics.h"
+
+#include <dlfcn.h>
+
+namespace
+{
+auto CheckLibrary(const char* soName) -> bool
+{
+    void* handle = dlopen(soName, RTLD_LAZY);
+    if (!handle) {
+        return false;
+    }
+    dlclose(handle);
+    return true;
+}
+} // anonymous namespace
+
+namespace nc::graphics
+{
+auto GetSupportedApis() -> std::span<const std::string_view>
+{
+    static const auto cachedApis = []()
+    {
+        auto supportedApis = std::vector<std::string_view>{};
+        supportedApis.reserve(2);
+
+        if (CheckLibrary("libvulkan.so"))
+            supportedApis.push_back(api::Vulkan);
+
+        supportedApis.push_back(api::OpenGL);
+        return supportedApis;
+    }();
+
+    return cachedApis;
+}
+} // namespace nc::graphics
