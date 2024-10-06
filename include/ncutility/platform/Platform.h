f7dcb566ae7fd7d6f6a75396fd2ef5c23b73b5da
McCallisterRomer <cal.romer@gmail.com>
2024-10-06 11:43:54 -0500

    warnings

--- include/ncutility/platform/Platform.h
+++ include/ncutility/platform/Platform.h
@@ -10,8 +10,6 @@
     #define NC_COMPILER_MSVC
 #elif defined(__GNUC__)
     #define NC_COMPILER_GCC
-#elif defined(__clang__)
-    #define NC_COMPILER_CLANG
 #endif
 
 #if defined(NC_COMPILER_MSVC)
@@ -20,7 +18,22 @@
 #elif defined(NC_COMPILER_GCC)
     #define NC_FORCE_INLINE __inline__ __attribute__((always_inline))
     #define NC_NO_INLINE __attribute__((noinline))
-#elif defined(NC_COMPILER_CLANG)
-    #define NC_FORCE_INLINE __inline__ __attribute__((always_inline))
-    #define NC_NO_INLINE __attribute__((noinline))
+#endif
+
+#if defined(NC_COMPILER_MSVC)
+    #define NC_PRAGMA(x) __pragma(x)
+    #define NC_DISABLE_WARNING_PUSH NC_PRAGMA(warning (push))
+    #define NC_DISABLE_WARNING_POP NC_PRAGMA(warning (pop))
+    #define NC_DISABLE_WARNING_MSVC(w) NC_PRAGMA(warning (disable : w))
+#else
+    #define NC_DISABLE_WARNING_MSVC(w)
+#endif
+
+#if defined(NC_COMPILER_GCC)
+    #define NC_PRAGMA(x) _Pragma(#x)
+    #define NC_DISABLE_WARNING_PUSH NC_PRAGMA(GCC diagnostic push)
+    #define NC_DISABLE_WARNING_POP NC_PRAGMA(GCC diagnostic pop)
+    #define NC_DISABLE_WARNING_GCC(w) NC_PRAGMA(GCC diagnostic ignored w)
+#else
+    #define NC_DISABLE_WARNING_GCC(w)
 #endif
