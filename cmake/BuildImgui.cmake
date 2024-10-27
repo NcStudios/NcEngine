bde4efc71b08e747ec5330a0a1e4caa58dcacf6c
McCallisterRomer <cal.romer@gmail.com>
2024-10-27 14:27:24 -0500

    gate on win32

--- cmake/BuildImgui.cmake
+++ cmake/BuildImgui.cmake
@@ -4,8 +4,14 @@ add_library(imgui STATIC
     ${imgui_SOURCE_DIR}/imgui_tables.cpp
     ${imgui_SOURCE_DIR}/imgui_widgets.cpp
     ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
-    ${imgui_SOURCE_DIR}/backends/imgui_impl_win32.cpp
 )
+
+if(WIN32)
+    target_sources(imgui
+        PRIVATE
+            ${imgui_SOURCE_DIR}/backends/imgui_impl_win32.cpp
+    )
+endif()
 
 target_compile_definitions(imgui
     PUBLIC
