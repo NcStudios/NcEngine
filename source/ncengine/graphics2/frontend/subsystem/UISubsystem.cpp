84823f97092b6e9f247f54b110e0c532a21fcba8
McCallisterRomer <cal.romer@gmail.com>
2024-10-28 05:35:40 -0500

    add source file for UISubsystem

---
+++ source/ncengine/graphics2/frontend/subsystem/UISubsystem.cpp
@@ -0,0 +1,35 @@
+#include "UISubsystem.h"
+
+#include "ncengine/ui/IUI.h"
+#include "ncengine/ui/editor/Editor.h"
+
+namespace nc::graphics
+{
+UISubsystem::UISubsystem(ecs::Ecs world,
+            ModuleProvider modules,
+            SystemEvents& events)
+    : m_editor{ui::editor::BuildEditor(world, modules, events)}
+{
+}
+
+UISubsystem::~UISubsystem() noexcept = default;
+
+auto UISubsystem::IsHovered() const noexcept -> bool
+{
+    return m_clientUI ? m_clientUI->IsHovered() : false;
+}
+
+void UISubsystem::SetClientUI(ui::IUI* ui) noexcept
+{
+    m_clientUI = ui;
+}
+
+void UISubsystem::UpdateUI(ecs::Ecs world)
+{
+    m_editor->Draw(world);
+    if (m_clientUI)
+    {
+        m_clientUI->Draw();
+    }
+}
+} // namespace nc::graphics
--- source/ncengine/graphics2/frontend/subsystem/UISubsystem.h
+++ source/ncengine/graphics2/frontend/subsystem/UISubsystem.h
@@ -3,39 +3,36 @@
 #include "ncengine/ui/IUI.h"
 #include "ncengine/ui/editor/Editor.h"
 
-namespace nc::graphics
+namespace nc
+{
+namespace ui
+{
+class IUI;
+
+namespace editor
+{
+class Editor;
+} // namespace editor
+} // namespace ui
+
+namespace graphics
 {
 class UISubsystem
 {
     public:
         UISubsystem(ecs::Ecs world,
                     ModuleProvider modules,
-                    SystemEvents& events)
-            : m_editor{ui::editor::BuildEditor(world, modules, events)}
-        {
-        }
+                    SystemEvents& events);
 
-        auto IsHovered() const noexcept -> bool
-        {
-            return m_clientUI ? m_clientUI->IsHovered() : false;
-        }
+        ~UISubsystem() noexcept;
 
-        void SetClientUI(ui::IUI* ui) noexcept
-        {
-            m_clientUI = ui;
-        }
-
-        void UpdateUI(ecs::Ecs world)
-        {
-            m_editor->Draw(world);
-            if (m_clientUI)
-            {
-                m_clientUI->Draw();
-            }
-        }
+        auto IsHovered() const noexcept -> bool;
+        void SetClientUI(ui::IUI* ui) noexcept;
+        void UpdateUI(ecs::Ecs world);
 
     private:
         std::unique_ptr<ui::editor::Editor> m_editor;
         ui::IUI* m_clientUI = nullptr;
 };
-} // namespace nc::graphics
+} // namespace graphics
+} // namespace nc
