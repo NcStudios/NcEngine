9d45d8296fa59d39803095f2c9bec65f8a1a29a8
McCallisterRomer <47375742+McCallisterRomer@users.noreply.github.com>
2024-12-21 15:48:25 -0600

    Consolidate Light Buffers (#852)
    * combine lights
    
    * cleanup
    
    * .
    
    * disambiguate widgets
    
    * remove stale types; add back todos

--- source/ncengine/graphics2/frontend/subsystem/LightSubsystem.cpp
+++ source/ncengine/graphics2/frontend/subsystem/LightSubsystem.cpp
@@ -15,7 +15,7 @@ constexpr float g_nearClip = 0.25f;
 constexpr float g_farClip = 96.0f;
 const auto g_lightProjectionMatrix = DirectX::XMMatrixPerspectiveRH(g_lightFieldOfView, 1.0f, g_nearClip, g_farClip);
 
-auto CalculateLightViewProjectionMatrix(const DirectX::XMMATRIX& transformMatrix) -> DirectX::XMMATRIX
+auto CalculateLightViewProjectionMatrix(DirectX::FXMMATRIX transformMatrix) -> DirectX::XMMATRIX
 {
     const auto look = DirectX::XMVector3Transform(DirectX::g_XMIdentityR2, transformMatrix);
     return DirectX::XMMatrixLookAtRH(transformMatrix.r[3], look, DirectX::g_XMNegIdentityR1) * g_lightProjectionMatrix;
@@ -29,7 +29,7 @@ constexpr float g_nearClip = 1.0f;
 constexpr float g_farClip = 100.f;
 const auto g_lightProjectionMatrix = DirectX::XMMatrixPerspectiveRH(g_lightFieldOfView, 1.0f, g_nearClip, g_farClip);
 
-auto CalculateLightViewProjectionMatrix(const DirectX::XMMATRIX& transformMatrix) -> DirectX::XMMATRIX
+auto CalculateLightViewProjectionMatrix(DirectX::FXMMATRIX transformMatrix) -> DirectX::XMMATRIX
 {
     const auto look = DirectX::XMVector3Transform(DirectX::g_XMIdentityR2, transformMatrix);
     return DirectX::XMMatrixLookAtRH(transformMatrix.r[3], look, DirectX::g_XMNegIdentityR1) * g_lightProjectionMatrix;
@@ -40,67 +40,50 @@ namespace nc::graphics
 {
 auto LightSubsystem::BuildState(ecs::ExplicitEcs<DirectionalLight, PointLight, SpotLight, Transform> ecs) -> LightRenderState
 {
+    m_data.clear();
+
     { // Directional Lights
-        const auto& lightPool = ecs.GetPool<DirectionalLight>();
-        const auto entities = lightPool.GetEntityPool();
-        m_directionalLights.clear();
-        m_directionalLights.reserve(entities.size());
-
-        for (auto [i, entity] : std::views::enumerate(entities))
+        const auto& pool = ecs.GetPool<DirectionalLight>();
+        for (auto [entity, light] : std::views::zip(pool.GetEntityPool(), pool.GetComponents()))
         {
-            auto& light = ecs.Get<DirectionalLight>(entity);
             auto& transform = ecs.Get<Transform>(entity);
-
-            m_directionalLights.emplace_back(light.color, transform.Forward());
+            m_data.emplace_back(light.color, transform.Forward());
         }
     }
 
     { // Point Lights
-        const auto& lightPool = ecs.GetPool<PointLight>();
-        const auto entities = lightPool.GetEntityPool();
-        m_pointLights.clear();
-        m_pointLights.reserve(entities.size());
-
-        for (auto [i, entity] : std::views::enumerate(entities))
+        const auto& pool = ecs.GetPool<PointLight>();
+        for (auto [entity, light] : std::views::zip(pool.GetEntityPool(), pool.GetComponents()))
         {
-            auto& light = ecs.Get<PointLight>(entity);
             auto& transform = ecs.Get<Transform>(entity);
-
-            m_pointLights.emplace_back(pointlight2::CalculateLightViewProjectionMatrix(transform.TransformationMatrix()),
-                                       transform.Position(),
-                                       0, /** @todo, come up with shadow decisioning (which lights cast shadows) */
-                                       light.diffuseColor,
-                                       light.radius);
+            m_data.emplace_back(
+                light.diffuseColor,
+                transform.Position(),
+                0, /** @todo, come up with shadow decisioning (which lights cast shadows) */
+                light.radius,
+                pointlight2::CalculateLightViewProjectionMatrix(transform.TransformationMatrix())
+            );
         }
     }
 
     { // Spot Lights
-        const auto& lightPool = ecs.GetPool<SpotLight>();
-        const auto entities = lightPool.GetEntityPool();
-        m_spotLights.clear();
-        m_spotLights.reserve(entities.size());
-
-        for (auto [i, entity] : std::views::enumerate(entities))
+        const auto& pool = ecs.GetPool<SpotLight>();
+        for (auto [entity, light] : std::views::zip(pool.GetEntityPool(), pool.GetComponents()))
         {
-            auto& light = ecs.Get<SpotLight>(entity);
             auto& transform = ecs.Get<Transform>(entity);
-
-            m_spotLights.emplace_back(spotlight2::CalculateLightViewProjectionMatrix(transform.TransformationMatrix()),
-                                      transform.Position(),
-                                      0, /** @todo, come up with shadow decisioning (which lights cast shadows) */
-                                      light.color,
-                                      light.innerAngle,
-                                      transform.Forward(),
-                                      light.outerAngle,
-                                      light.radius);
+            m_data.emplace_back(
+                light.color,
+                transform.Position(),
+                light.innerAngle,
+                transform.Forward(),
+                light.outerAngle,
+                light.radius,
+                0, /** @todo, come up with shadow decisioning (which lights cast shadows) */
+                spotlight2::CalculateLightViewProjectionMatrix(transform.TransformationMatrix())
+            );
         }
     }
 
-    return LightRenderState
-    {
-        m_directionalLights,
-        m_pointLights,
-        m_spotLights
-    };
+    return LightRenderState{m_data};
 }
 } // namespace nc::graphics
