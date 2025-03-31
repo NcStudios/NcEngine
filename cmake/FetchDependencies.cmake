f3d01acf4c8abe0abfa6d2e9a9c1e5e573ac7f88
McCallisterRomer <cal.romer@gmail.com>
2025-03-31 14:10:15 -0500

    patch

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -59,8 +59,8 @@ set(TF_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(TF_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 
 FetchContent_Declare(taskflow
-                     GIT_REPOSITORY https://github.com/taskflow/taskflow.git
-                     GIT_TAG        v3.9.0
+                     GIT_REPOSITORY https://github.com/NcStudios/taskflow.git
+                     GIT_TAG        v3.9.0+nc.1
                      GIT_SHALLOW    TRUE
 )
 
