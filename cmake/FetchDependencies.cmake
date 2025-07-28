352c45a4b1d88b34509455c58b875a4558ae5026
McCallisterRomer <cal.romer@gmail.com>
2025-07-28 05:08:12 -0500

    use forked version

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -49,8 +49,8 @@ FetchContent_Declare(imgui
 )
 
 FetchContent_Declare(imguizmo
-                     GIT_REPOSITORY https://github.com/CedricGuillemet/ImGuizmo.git
-                     GIT_TAG        1.83
+                     GIT_REPOSITORY https://github.com/NcStudios/ImGuizmo.git
+                     GIT_TAG        v1.83+nc
                      GIT_SHALLOW    TRUE
 )
 
@@ -58,7 +58,6 @@ FetchContent_MakeAvailable(glfw imgui imguizmo)
 
 # Dear ImGui target needs to be available before diligent. Also, it doesn't have a cmake lists, so we add it via script.
 include(cmake/AddImguiTarget.cmake)
-# include(cmake/AddImGuizmoTarget.cmake)
 disable_warnings_for_headers(imgui)
 
 # Taskflow
