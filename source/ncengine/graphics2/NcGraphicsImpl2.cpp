f79a5dd286bc17b80167fbf1571e3972fc6157ed
Jaremie Romer <33360084+jaremieromer@users.noreply.github.com>
2025-08-10 18:12:47 -0400

    ImGui images (#941)

--- source/ncengine/graphics2/NcGraphicsImpl2.cpp
+++ source/ncengine/graphics2/NcGraphicsImpl2.cpp
@@ -13,6 +13,7 @@
 #include "ncengine/scene/NcScene.h"
 #include "ncengine/task/TaskGraph.h"
 #include "ncengine/utility/Log.h"
+#include "ncengine/ui/UIImage.h"
 
 #include "imgui.h"
 #include "DirectXMath.h"
@@ -371,6 +372,10 @@ NcGraphicsImpl2::NcGraphicsImpl2(const config::GraphicsSettings& graphicsSetting
           m_numSamples{m_engine.GetDeviceCapability().msaaSampleCount},
           m_isMinimized{false}
 {
+    ui::SetUIImageProvider([this](uint32_t index) -> ImTextureID
+    {
+        return m_shaderBindings.GetPerFrameSignature().GetTextureBuffer().GetImGuiHandle(index);
+    });
 }
 
 NcGraphicsImpl2::~NcGraphicsImpl2()
