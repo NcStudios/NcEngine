ae0493d1a157a72395da6b1936431486fa812a9c
McCallisterRomer <cal.romer@gmail.com>
2025-08-15 10:17:47 -0500

    code review

--- include/ncmath/Color.h
+++ include/ncmath/Color.h
@@ -23,12 +23,12 @@ struct Color : public Vector4
     {
     }
 
-    static constexpr auto Red()   -> Color { return Color{1.0f, 0.0, 0.0f, 1.0f}; }
-    static constexpr auto Green() -> Color { return Color{0.0f, 1.0, 0.0f, 1.0f}; }
-    static constexpr auto Blue()  -> Color { return Color{0.0f, 0.0, 1.0f, 1.0f}; }
-    static constexpr auto White() -> Color { return Color{1.0f, 1.0, 1.0f, 1.0f}; }
-    static constexpr auto Black() -> Color { return Color{0.0f, 0.0, 0.0f, 1.0f}; }
-    static constexpr auto Clear() -> Color { return Color{0.0f, 0.0, 0.0f, 0.0f}; }
+    static constexpr auto Red()         -> Color { return Color{1.0f, 0.0, 0.0f, 1.0f}; }
+    static constexpr auto Green()       -> Color { return Color{0.0f, 1.0, 0.0f, 1.0f}; }
+    static constexpr auto Blue()        -> Color { return Color{0.0f, 0.0, 1.0f, 1.0f}; }
+    static constexpr auto White()       -> Color { return Color{1.0f, 1.0, 1.0f, 1.0f}; }
+    static constexpr auto Black()       -> Color { return Color{0.0f, 0.0, 0.0f, 1.0f}; }
+    static constexpr auto Transparent() -> Color { return Color{0.0f, 0.0, 0.0f, 0.0f}; }
 
     constexpr auto R()       ->       float& { return x; }
     constexpr auto G()       ->       float& { return y; }
